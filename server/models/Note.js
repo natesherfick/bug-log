31f4c878dd116721f71557d37619c20b9ac3f397
Nate Sherfick <natesherfick@gmail.com>
2020-04-27 12:52:03 -0600

    server running

---
+++ server/models/Note.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId
+
+
+const Note = new Schema({
+  content: { type: String, required: true },
+  bug: { type: ObjectId, ref: 'Bug', required: true },
+  flagged: { type: String, enum: ["pending", "completed", "rejected"] },
+  creatorEmail: { type: String, required: true }
+}, { timestamps: true, toJSON: { virtuals: true } })
+
+
+
+export default Note
